90403bf4e3d9e8c2f3768a499cc5a59de5d58810
Jolonto <aluyapeter7@gmail.com>
2025-09-02 22:44:36 +0100

    list command done

--- cmd/list.go
+++ cmd/list.go
@@ -1,32 +1,89 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/aluyapeter/williamsgov/models"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List all tasks",
+	Long: `Used to display all your tasks that are still marked as incomplete. Use the --all flag to show both incomplete and completed task`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		taskList, err := models.LoadTasks()
+		if err != nil {
+			fmt.Printf("Error loading tasks: %v\n", err)
+			return
+		}
+
+		// if len(taskList.Tasks) == 0 {
+		// 	fmt.Println("No task found")
+		// 	return
+		// }
+
+		// fmt.Println("you have the following tasks:")
+		// for i, task := range taskList.Tasks {
+		// 	status := " "
+		// 	if task.Completed {
+		// 		status = "done"
+		// 	}
+		// 	fmt.Printf("%s %d. %s\n", status, i+1, task.Title)
+		// }
+		if len(taskList.Tasks) == 0 {
+			fmt.Println("No tasks found. Add some tasks to get started using 'williamsgo add \"your task here\"'")
+			return
+		}
+
+		showAll, _ := cmd.Flags().GetBool("all")
+
+		fmt.Println("Your tasks:")
+
+		displayedCount := 0
+		for _, task := range taskList.Tasks{
+			if task.Completed && !showAll  {
+				continue
+			}
+
+			status := "Pending"
+			if task.Completed {
+				status = "Completed"
+			}
+
+			fmt.Printf("\n [ID: %d] %s\n", task.ID, task.Title)
+			fmt.Printf("Status: %s\n", status)
+
+			if task.Description != "" {
+				fmt.Printf("Description: %s\n", task.Description)
+			}
+
+			fmt.Printf("   📅 Created: %s\n", task.CreatedAt.Format("2006-01-02 15:04:05"))
+			
+			if task.Completed && task.CompletedAt != nil {
+				fmt.Printf("   ✅ Completed: %s\n", task.CompletedAt.Format("2006-01-02 15:04:05"))
+			}
+
+			displayedCount++
+		}
+
+		fmt.Printf("\n showing %d task(s)", displayedCount)
+		if !showAll {
+			fmt.Println("(use --all to show completed tasks)")
+		} else {
+			fmt.Println()
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().BoolP("all", "a", false, "Show all tasks including completed ones")
 
 	// Here you will define your flags and configuration settings.
 
